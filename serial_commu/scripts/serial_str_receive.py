8f9549327e159c56454d1e905e7b840e5d9b4750
JedtanutSem <61010167@kmitl.ac.th>
2021-12-26 23:11:24 -0500

    Add serial read analog control joystick

---
+++ serial_commu/scripts/serial_str_receive.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import serial
+import rospy
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+import time
+import json
+max_linear_speed = 0.22
+max_angular_speed = 0.22
+
+if __name__ == "__main__":
+
+    arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1)
+
+    #rospy.Subscriber('chatter', String, callback)
+    pub = rospy.Publisher('serial_read', String, queue_size=10)
+    pub_vel = rospy.Publisher('cmd_vel',Twist,queue_size=10)
+    rospy.init_node('read_ser', anonymous=True)
+    rate = rospy.Rate(190) # 10hz
+
+    while not rospy.is_shutdown():
+
+        #str = '%s,%s\n' %(get_a,get_b)
+        #rospy.loginfo(str)
+        #arduino.write(str)
+        data = arduino.readline()[:-2]
+
+        if data:
+            serial_str = data
+            serial_split = serial_str.split(',')
+            #rospy.loginfo(len(serial_split))
+            try:
+                if len(serial_split) == 2:
+                    x_linear_val = serial_split[0]
+                    x_linear_val = int(x_linear_val)
+                    x_linear_vel =  max_linear_speed * (x_linear_val / 100)
+
+                    z_angular_val = serial_split[1]
+                    z_angular_val = int(z_angular_val)
+                    z_angular_vel = max_angular_speed * (z_angular_val / 100)
+
+
+                    msg = Twist()
+                    msg.linear.x = x_linear_vel
+                    msg.angular.z = z_angular_vel
+                    pub_vel.publish(msg)
+            except:
+                pass
+
+            rospy.loginfo(serial_str)
+            pub.publish(serial_str)
+        rate.sleep()
