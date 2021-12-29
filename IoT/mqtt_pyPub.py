f4cd2afeba89e00474708f7fc268000a4bc89664
JedtanutSem <94428679+JedtanutSem@users.noreply.github.com>
2021-12-29 22:30:59 +0700

    [Add] mqtt python publish

---
+++ IoT/mqtt_pyPub.py
@@ -0,0 +1,18 @@
+import paho.mqtt.client as mqtt
+import json
+import time
+i = 0
+B = 5
+host = "192.168.1.45"
+port = 8000
+client = mqtt.Client()
+client.connect(host)
+#data = "["Test"]"
+
+while i != 10:
+    MQTT_MSG = json.dumps({"A": i, "C": 7, "G": "44.5", "petalWidth": "1.5"});
+    client.publish("data",MQTT_MSG)
+    time.sleep(1)
+    i = i+1
+    if(i == 9):
+        i = 0
