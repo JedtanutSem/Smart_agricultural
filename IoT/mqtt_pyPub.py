85fa1d9a23a65d5e44267198378464e7f2963dc8
JedtanutSem <61010167@kmitl.ac.th>
2021-12-30 22:39:01 +0700

    [Update] test comment ros

--- IoT/mqtt_pyPub.py
+++ IoT/mqtt_pyPub.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import json
 import time
+#import rospy
 i = 0
 B = 5
 host = "192.168.1.45"
@@ -12,6 +13,7 @@ client.connect(host)
 while i != 10:
     MQTT_MSG = json.dumps({"A": i, "C": 7, "G": "44.5", "petalWidth": "1.5"});
     client.publish("data",MQTT_MSG)
+    #rospy.loginfo(MQTT_MSG)
     time.sleep(1)
     i = i+1
     if(i == 9):
