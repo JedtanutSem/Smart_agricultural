7f045a55dbaa143cd6e505ad656bf82407f4a029
JedtanutSem <61010167@kmitl.ac.th>
2021-12-30 23:54:09 +0700

    [Add] ros mqtt publish

---
+++ iot_ros/scripts/mqtt_rosPub.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import paho.mqtt.client as mqtt
+import json
+import time
+import rospy
+from geometry_msgs.msg import Twist
+i = 0
+B = 5
+vel_x = 0
+ang_z = 0
+host = "10.66.11.150"
+port = 1883
+client = mqtt.Client()
+client.connect(host)
+#data = "["Test"]"
+
+def vel_blk(vel):
+    global vel_x
+    global ang_z
+    vel_x = vel.linear.x
+    ang_z = vel.angular.z
+    #rospy.loginfo(vel_x)
+
+
+rospy.init_node('mqtt_vel', anonymous = True)
+rospy.Subscriber('cmd_vel', Twist, vel_blk)
+rate = rospy.Rate(10)
+
+while not rospy.is_shutdown():
+    MQTT_MSG = json.dumps({"vel_x": vel_x, "ang_z": ang_z});
+    #MQTT_MSG = vel_x
+    client.publish("vel_x",vel_x)
+    client.publish("ang_z",ang_z)
+    #rospy.loginfo(MQTT_MSG)
+    #time.sleep(1
+    rate.sleep()
