0213894a620b6f4bb153eaa1ac87869dbc3ed546
JedtanutSem <61010167@kmitl.ac.th>
2022-01-25 16:05:15 +0700

    [Delete]

--- open_man_tc/open_manipulator_tc/notas.sh
+++
@@ -1,6 +0,0 @@
-reset;roslaunch open_manipulator_moveit_config moveit_planning_execution.launch sim:=false
-
-rosrun open_manipulator_core platform_pick_and_place.py
-
-
-rostopic echo /gripper_command
